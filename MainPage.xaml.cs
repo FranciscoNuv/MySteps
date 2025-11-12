7b9d41fd843c870d625a0f20190a534a14f46b54
Francisco_Ripardo_NUVIO <francisco.ripardo@nuvio.com.br>
2025-11-12 13:12:54 -0400

    add internalization

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,20 @@
-﻿namespace MySteps
+﻿﻿using MySteps.Resources;
+using System.Globalization;
+using LocalizationResourceManager.Maui;
+
+namespace MySteps
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        ILocalizationResourceManager _resourceManager;
 
-        public MainPage()
+        public MainPage(ILocalizationResourceManager resourceManager)
         {
             InitializeComponent();
+            _resourceManager = resourceManager;
+            // _resourceManager.CurrentCulture = new(() => resourceManager.CurrentCulture.NativeName);
+            BindingContext = this;
         }
 
         private void OnCounterClicked(object? sender, EventArgs e)
@@ -19,6 +27,22 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnPtClicked(object sender, EventArgs e)
+        {
+            // var languages = new List<string>() { "en", "pt" };
+            // var culture = resourceManager.CurrentCulture;
+            // var index = languages.IndexOf("pt");
+            _resourceManager.CurrentCulture = new CultureInfo("pt");
+        }
+
+        private void OnEnClicked(object sender, EventArgs e)
+        {
+            // var languages = new List<string>() { "en", "pt" };
+            // var culture = resourceManager.CurrentCulture;
+            // var index = languages.IndexOf("pt");
+            _resourceManager.CurrentCulture = new CultureInfo("en");
         }
     }
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿﻿using LocalizationResourceManager.Maui;
+using MySteps.Resources;
+using Microsoft.Extensions.Logging;
 
 namespace MySteps
 {
@@ -13,7 +15,14 @@ namespace MySteps
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .UseLocalizationResourceManager(settings =>
+                {
+                    settings.AddResource(AppResources.ResourceManager);
+                    settings.RestoreLatestCulture(true);
                 });
+                
+            builder.Services.AddSingleton<MainPage>();
 
 #if DEBUG
     		builder.Logging.AddDebug();
